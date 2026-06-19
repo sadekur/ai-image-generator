894383e31471048c804b34ef343248d08e5471c3
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-19 23:17:21 +0600

    feat: refactor App component to use styled Container and Wrapper for layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import styled, { ThemeProvider } from 'styled-components';
 import {darkTheme} from './theme';
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(theme) => theme.bg};
+  color: ${(theme) => theme.text};
+`;
+
+const Wrapper = styled.div`
+  background-color: ${(theme) => theme.card};
+  padding: 20px;
+  border-radius: 10px;
+`;
 function App() {
   return <ThemeProvider theme={darkTheme}>
-    <div>
-      Hello World!
-    </div>
+    <Container>
+      <Wrapper>
+        Hello World!
+      </Wrapper>
+    </Container>
   </ThemeProvider>;
 }
 
