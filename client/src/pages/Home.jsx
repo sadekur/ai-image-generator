c250fa3d77474592ddeb07167306404a5f79f34f
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-20 23:34:52 +0600

    refactor: remove styled-components for Home page and simplify layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,65 +1,8 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
 import SearchBar from "../components/SearchBar";
 import ImageCard from "../components/cards/ImageCard";
 import { GetPosts } from "../api";
 import { CircularProgress } from "@mui/material";
-
-const Container = styled.div`
-  padding: 30px 30px;
-  padding-bottom: 200px;
-  height: 100%;
-  overflow-y: scroll;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  @media (max-width: 768px) {
-    padding: 6px 10px;
-  }
-  background: ${({ theme }) => theme.background};
-`;
-
-const HeadLine = styled.div`
-  font-size: 34px;
-  font-weight: 500;
-  color: ${({ theme }) => theme.text_primary};
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const Span = styled.div`
-  font-size: 30px;
-  font-weight: 800;
-  color: ${({ theme }) => theme.secondary};
-`;
-
-const Wrapper = styled.div`
-  width: 100%;
-  max-width: 1400px;
-  padding: 32px 0px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CardWrapper = styled.div`
-  display: grid;
-  gap: 20px;
-
-  @media (min-width: 1200px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-
-  @media (min-width: 640px) and (max-width: 1199px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  @media (max-width: 639px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-`;
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
