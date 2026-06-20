e606559c5e5a33a2d92c47184403e08653c32514
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-20 22:21:53 +0600

    feat: add file-saver dependency and enhance SearchBar component with styled container

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,10 +6,11 @@ import { GetPosts } from "../api";
 import { CircularProgress } from "@mui/material";
 
 const Container = styled.div`
-  padding: 30px 30px;
-  padding-bottom: 200px;
   height: 100%;
   overflow-y: scroll;
+  background: ${({ theme }) => theme.bg};
+  padding: 30px 30px;
+  padding-bottom: 50px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -17,22 +18,28 @@ const Container = styled.div`
   @media (max-width: 768px) {
     padding: 6px 10px;
   }
-  background: ${({ theme }) => theme.background};
 `;
 
-const HeadLine = styled.div`
+const Headline = styled.div`
   font-size: 34px;
   font-weight: 500;
   color: ${({ theme }) => theme.text_primary};
   display: flex;
   align-items: center;
   flex-direction: column;
-`;
 
+  @media (max-width: 600px) {
+    font-size: 22px;
+  }
+`;
 const Span = styled.div`
   font-size: 30px;
   font-weight: 800;
   color: ${({ theme }) => theme.secondary};
+
+  @media (max-width: 600px) {
+    font-size: 20px;
+  }
 `;
 
 const Wrapper = styled.div`
@@ -41,21 +48,17 @@ const Wrapper = styled.div`
   padding: 32px 0px;
   display: flex;
   justify-content: center;
-  align-items: center;
 `;
 
 const CardWrapper = styled.div`
   display: grid;
   gap: 20px;
-
   @media (min-width: 1200px) {
     grid-template-columns: repeat(4, 1fr);
   }
-
   @media (min-width: 640px) and (max-width: 1199px) {
     grid-template-columns: repeat(3, 1fr);
   }
-
   @media (max-width: 639px) {
     grid-template-columns: repeat(2, 1fr);
   }
@@ -66,15 +69,15 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [search, setSearch] = useState("");
-  const [filteredPost, setFilteredPost] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
   const getPosts = async () => {
     setLoading(true);
     await GetPosts()
       .then((res) => {
-        setPosts(res?.data?.data);
-        setFilteredPost(res?.data?.data);
         setLoading(false);
+        setPosts(res?.data?.data);
+        setFilteredPosts(res?.data?.data);
       })
       .catch((error) => {
         setError(error?.response?.data?.message);
@@ -86,49 +89,52 @@ const Home = () => {
     getPosts();
   }, []);
 
+  //Search
   useEffect(() => {
     if (!search) {
-      setFilteredPost(posts);
+      setFilteredPosts(posts);
     }
-    const filteredPosts = posts.filter((post) => {
-      const promptMatch = post?.prompt?.toLowerCase().includes(search);
-      const authorMatch = post?.author?.toLowerCase().includes(search);
+
+    const SearchFilteredPosts = posts.filter((post) => {
+      const promptMatch = post?.prompt
+        ?.toLowerCase()
+        .includes(search.toString().toLowerCase());
+      const authorMatch = post?.name
+        ?.toLowerCase()
+        .includes(search.toString().toLowerCase());
 
       return promptMatch || authorMatch;
     });
 
     if (search) {
-      setFilteredPost(filteredPosts);
+      setFilteredPosts(SearchFilteredPosts);
     }
   }, [posts, search]);
 
   return (
     <Container>
-      <HeadLine>
+      <Headline>
         Explore popular posts in the Community!
-        <Span>⦾ Generated with AI ⦾</Span>
-      </HeadLine>
-      <SearchBar
-        search={search}
-        handleChange={(e) => setSearch(e.target.value)}
-      />
+        <Span>⦿ Generated with AI ⦿</Span>
+      </Headline>
+      <SearchBar search={search} setSearch={setSearch} />
       <Wrapper>
         {error && <div style={{ color: "red" }}>{error}</div>}
         {loading ? (
           <CircularProgress />
         ) : (
           <CardWrapper>
-            {filteredPost.length > 0 ? (
+            {filteredPosts.length === 0 ? (
+              <>No Posts Found</>
+            ) : (
               <>
-                {filteredPost
+                {filteredPosts
                   .slice()
                   .reverse()
                   .map((item, index) => (
                     <ImageCard key={index} item={item} />
                   ))}
               </>
-            ) : (
-              <>No Posts Found !!</>
             )}
           </CardWrapper>
         )}
