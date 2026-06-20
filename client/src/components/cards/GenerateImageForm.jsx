fb77bf6fd1e1d141dcb211a10754c75ef6619e5f
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-20 22:53:44 +0600

    feat: replace GeneratedImageCard component with GenerateImageForm for improved image generation handling

--- client/src/components/cards/GenerateImageForm.jsx
+++ client/src/components/cards/GenerateImageForm.jsx
@@ -1,45 +1,136 @@
-import { CircularProgress } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Button from "./button";
+import TextInput from "./TextInput";
+import { AutoAwesome, CreateRounded } from "@mui/icons-material";
+import { CreatePost, GenerateAIImage } from "../api";
 
-const Container = styled.div`
+const Form = styled.div`
   flex: 1;
-  min-height: 300px;
+  padding: 16px 20px;
   display: flex;
-  gap: 16px;
-  align-items: center;
+  flex-direction: column;
+  gap: 9%;
   justify-content: center;
-  padding: 16px;
-  border: 2px dashed ${({ theme }) => theme.yellow};
-  color: ${({ theme }) => theme.arrow + 80};
-  border-radius: 20px;
+`;
+const Top = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+const Title = styled.div`
+  font-size: 28px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
+`;
+const Desc = styled.div`
+  font-size: 17px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary};
+`;
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary};
+`;
+const Actions = styled.div`
+  flex: 1;
+  display: flex;
+  gap: 8px;
 `;
 
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-radius: 24px;
-  background: ${({ theme }) => theme.black + 50};
-`;
-
-const GeneratedImageCard = ({ src, loading }) => {
+const GenerateImageForm = ({
+  post,
+  setPost,
+  createPostLoading,
+  setGenerateImageLoading,
+  generateImageLoading,
+  setCreatePostLoading,
+}) => {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const generateImageFun = async () => {
+    setGenerateImageLoading(true);
+    await GenerateAIImage({ prompt: post.prompt })
+      .then((res) => {
+        setPost({
+          ...post,
+          photo: `data:image/jpge;base64,${res?.data?.photo}`,
+        });
+        setGenerateImageLoading(false);
+      })
+      .catch((error) => {
+        setError(error?.response?.data?.message);
+        setGenerateImageLoading(false);
+      });
+  };
+  const createPostFun = async () => {
+    setCreatePostLoading(true);
+    await CreatePost(post)
+      .then((res) => {
+        setCreatePostLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error?.response?.data?.message);
+        setCreatePostLoading(false);
+      });
+  };
   return (
-    <Container>
-      {loading ? (
-        <>
-          <CircularProgress
-            style={{ color: "inherit", width: "24px", height: "24px" }}
-          />
-          Generating Your Image ...
-        </>
-      ) : (
-        <>
-          {src ? <Image src={src} /> : <>Write a prompt to generate image </>}
-        </>
-      )}
-    </Container>
+    <Form>
+      <Top>
+        <Title>Generate Image with prompt</Title>
+        <Desc>
+          Write your prompt according to the image you want to generate!
+        </Desc>
+      </Top>
+      <Body>
+        <TextInput
+          label="Author"
+          placeholder="Enter your name.."
+          name="name"
+          value={post.name}
+          handelChange={(e) => setPost({ ...post, name: e.target.value })}
+        />
+        <TextInput
+          label="Image Prompt"
+          placeholder="Write a detailed prompt about the image . . . "
+          name="name"
+          rows="8"
+          textArea
+          value={post.prompt}
+          handelChange={(e) => setPost({ ...post, prompt: e.target.value })}
+        />
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        ** You can post the AI Generated Image to the Community **
+      </Body>
+      <Actions>
+        <Button
+          text="Generate Image"
+          flex
+          leftIcon={<AutoAwesome />}
+          isLoading={generateImageLoading}
+          isDisabled={post.prompt === ""}
+          onClick={() => generateImageFun()}
+        />
+        <Button
+          text="Post Image"
+          flex
+          type="secondary"
+          leftIcon={<CreateRounded />}
+          isLoading={createPostLoading}
+          isDisabled={
+            post.name === "" || post.prompt === "" || post.photo === ""
+          }
+          onClick={() => createPostFun()}
+        />
+      </Actions>
+    </Form>
   );
 };
 
-export default GeneratedImageCard;
+export default GenerateImageForm;
