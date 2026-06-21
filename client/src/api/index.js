7f212a989193333cbdc7443341580237502d0fee
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-21 22:13:23 +0600

    fix: correct API endpoint paths for posts and update import in Posts controller

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,7 +5,7 @@ const API = axios.create({
   baseURL: "http://localhost:8080/api/",
 });
 
-export const GetPosts = async () => await API.get("/post/");
-export const CreatePost = async (data) => await API.post("/post/", data);
+export const GetPosts = async () => await API.get("/posts/");
+export const CreatePost = async (data) => await API.post("/posts/", data);
 export const GenerateImageFromPrompt = async (data) =>
   await API.post("/generateImage/", data);
--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -1,4 +1,4 @@
-import Post from '../models/Post.js';
+import Post from '../models/Posts.js';
 import * as dotenv from 'dotenv';
 import { createError } from '../error.js';
 
