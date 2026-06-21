f50f3268105e815bdc3d230f9021d402f6fff940
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-21 22:03:56 +0600

    feat: add express router and import post controllers in Posts.js

---
+++ server/routes/Posts.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getAllPosts, createPost } from "../controllers/Posts.js";
+
+const router = express.Router();
+
+router.get("/", getAllPosts);
+router.post("/", createPost);
