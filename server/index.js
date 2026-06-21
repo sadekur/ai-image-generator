e5f294cb28107122682f4a2ada666cc3d8570c6e
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-21 21:38:42 +0600

    refactor: remove mongoose connection from server startup and adjust server initialization

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+// import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
