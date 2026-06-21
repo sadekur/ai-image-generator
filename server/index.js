7ba615fc8cf55fcc7343a0f6dbf2d3a474525043
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-21 21:33:04 +0600

    fix: ensure express.urlencoded middleware is applied in index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
     res.status(200).send({
