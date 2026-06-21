d4d5ecb8c83ef67ac5152ebb1c21b4cdb0c56686
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-21 22:19:19 +0600

    feat: update createPost function to handle additional post creation logic

--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -4,6 +4,13 @@ import { createError } from '../error.js';
 
 dotenv.config();
 
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 export const getPosts = async (req, res, next) => {
     try {
         const posts = await Post.find();
