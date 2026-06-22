62d48dbf6b7a64020840c36a4329f63f2c4e7d27
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-22 20:36:28 +0600

    feat: add OpenAI configuration to GenerateAIImage controller

--- server/controllers/GenerateAIImage.jsx
+++ server/controllers/GenerateAIImage.jsx
@@ -1,6 +1,13 @@
 import * as dotenv from 'dotenv';
 import { createError } from '../error.js';
+import { Configuration, OpenAI, OpenAIApi } from "openai";
+
 
 dotenv.config();
 
 // Setup OpenAI configuration
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
